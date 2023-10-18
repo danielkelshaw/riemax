18227bba3d7bd952e9a3f28ba6fff18e41d0f438
danielkelshaw <daniel.j.kelshaw@gmail.com>
2023-10-18 22:48:29 +0100

    Provided riemax.numerical.integrators
    Provided `riemax.numerical.integrators`, which implements a number of integration
    schemes for time-integration. On the whole, these do not provide custom `jvp` rules
    so use standard autodiff, rather then computing the adjoint. We do, however, provide
    an implementation of `odeint` which does compute the adjoint.
    
    Also, provided a number of integral approximation tools in `riemax.numerical.integral_approximators`.

--- src/riemax/numerical/__init__.py
+++ src/riemax/numerical/__init__.py
@@ -1,1 +1,2 @@
+from . import integrators
 from .newton_raphson import NewtonConvergenceState, NewtonRaphsonParams, newton_raphson
---
+++ src/riemax/numerical/integral_approximators.py
@@ -0,0 +1,60 @@
+import typing as tp
+
+import jax
+import jax.numpy as jnp
+
+type IntegralApproximationFn = tp.Callable[[jax.Array, float], jax.Array]
+
+
+def mean_integration(fx: jax.Array, _: float) -> jax.Array:
+
+    """Integration by taking mean.
+
+    Parameters:
+        fx: vector of function evaluations to integrate
+        h: sample spacing
+
+    Returns:
+        integrated value
+    """
+
+    if fx.ndim != 1:
+        raise ValueError('Must pass a 1D array to integrate.')
+
+    return jnp.mean(fx)
+
+
+def trapezoidal_integration(fx: jax.Array, h: float) -> jax.Array:
+
+    """Integration by trapezoidal rule.
+
+    Parameters:
+        fx: vector of function evaluations to integrate
+        h: sample spacing
+
+    Returns:
+        integrated value
+    """
+
+    if fx.ndim != 1:
+        raise ValueError('Must pass a 1D array to integrate.')
+
+    return (h / 2.0) * (fx[0] + fx[-1] + 2.0 * jnp.sum(fx[1:-1]))
+
+
+def simpsons_integration(fx: jax.Array, h: float) -> jax.Array:
+
+    """Integration by Simpsons 1/3 rule.
+
+    Parameters:
+        fx: vector of function evaluations to integrate
+        h: sample spacing
+
+    Returns:
+        integrated value
+    """
+
+    if fx.ndim != 1:
+        raise ValueError('Must pass a 1D array to integrate()')
+
+    return (h / 3.0) * (fx[0] + fx[-1] + 4.0 * jnp.sum(fx[1:-1:2]) + 2.0 * jnp.sum(fx[2:-1:2]))
