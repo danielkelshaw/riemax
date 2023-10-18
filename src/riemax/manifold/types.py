d50c5fe2fd9ca90ccacb56705624d24fadc30a4e
danielkelshaw <daniel.j.kelshaw@gmail.com>
2023-10-18 16:35:22 +0100

    Provided implementation of geometric tools
    Provided `src/riemax/manifold/geometry.py`, a collection of code for computing intrinsic quantities on the manifold,
    this includes: metric tensors, curvatures, inner products, and more...
    
    Also provided configuration for `mkdocs` allowing for generation of a documentation website.

---
+++ src/riemax/manifold/types.py
@@ -0,0 +1,22 @@
+import typing as tp
+
+import jax
+
+type M[T] = T
+type TpM[T] = T
+type Rn[T] = T
+
+type MetricFn = tp.Callable[[M[jax.Array]], jax.Array]
+
+
+class TangentSpace[T](tp.NamedTuple):
+
+    r"""Representation of the tangent space on the manifold
+
+    Parameters:
+        point: position $p \in M$
+        vector: corresponding vector, $v \in T_p M$
+    """
+
+    point: M[T]
+    vector: TpM[T]
